753f95a56e535eb9b63a83e1215e7a4fa38c5554
dallinbergquist <dallinbergquist@gmail.com>
2016-02-10 05:54:32 +0000

    LAB3

---
+++ inClass/lab3/avatar.js
@@ -0,0 +1,40 @@
+angular.module('app',[])
+.controller('mainCtrl',mainCtrl)
+.directive('avatar',avatarDirective);
+
+function mainCtrl($scope){
+  $scope.users=[{
+    name:'Mikey Murphy',avatarUrl:'https://pbs.twimg.com/profile_images/621816592519688192/U8sbRNik.jpg'
+  }];
+
+  $scope.addNew=function(user){
+    $scope.users.push({
+      name:user.name,
+      avatarUrl:user.url
+    });
+    user.name='';
+    user.url='';
+  };
+}
+
+function avatarDirective(){
+  return{
+    scope:{
+      user:'='
+    },
+    restrict:'E',
+    template:(
+      '<div class="Avatar">'+
+      '<img ng-src="{{user.avatarUrl}}" />'+
+      '<h4>{{user.name}}</h4>'+
+      '</div>'
+    ),
+    link: link
+  };
+
+  function link(scope){
+    if(!scope.user.avatarUrl){
+      scope.user.avatarUrl='http://thealmanac.org/assets/img/default_avatar.png';
+    }
+  }
+}
